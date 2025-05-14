ccea6bb1628b65df515e01a48f8f56b0e713acb7
Ethan <emcannaert@ucdavis.edu>
2025-05-14 12:05:37 -0500

    updating signal SF lib

--- postprocess/return_signal_SF/return_signal_SF.py
+++ postprocess/return_signal_SF/return_signal_SF.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 # overloaded version to work for the individual decays 
 def return_signal_SF(year,mass_point,decay, y_uu = 2.0, y_x = 2.0):    ## parameters are theory couplings
 	## input signal mass point in the format Suu$Mass_chi$Mass
@@ -125,14 +126,15 @@ def return_Suu_to_chi_chi_xs(mass_point,decay, y_uu = 2.0, y_x = 2.0):
 	if "chi3" in mass_point: chi_mass = 3000
 
 	# the production xs of Suu depends on y_uu^2, all values below are reference values for y_uu = 2
-	Suu_prod_xs = { "4000": 1000.  * (pow(1.0*y_uu,2) / pow(2.0,2) ),   #fb
-					"5000": 500.   * (pow(1.0*y_uu,2) / pow(2.0,2) ),   #fb
-					"6000": 200.   * (pow(1.0*y_uu,2) / pow(2.0,2) ),   #fb  
-					"7000": 28     * (pow(1.0*y_uu,2) / pow(2.0,2) ),   #fb
-					"8000": 3.5    * (pow(1.0*y_uu,2) / pow(2.0,2) )    #fb 
+	Suu_prod_xs = { "4000": 32.0  * (pow(1.0*y_uu,2) / pow(0.2,2) ),   #fb
+					"5000": 6.95   * (pow(1.0*y_uu,2) / pow(0.2,2) ),   #fb
+					"6000": 1.37   * (pow(1.0*y_uu,2) / pow(0.2,2) ),   #fb  
+					"7000": 0.231     * (pow(1.0*y_uu,2) / pow(0.2,2) ),   #fb
+					"8000": 0.0303    * (pow(1.0*y_uu,2) / pow(0.2,2) )    #fb
 	}
 
-	collected_data = {"2015":19.52,"2016":16.81, "2017":41.48, "2018":59.83}  # collected luminosity per year 
+
+	collected_data = {"2015":19.5,"2016":16.80, "2017":41.4, "2018":59.8}  # collected luminosity per year 
 
 	WB_BR = 0.50
 	ZT_BR = 0.25
@@ -156,27 +158,27 @@ def return_Suu_to_chi_chi_xs(mass_point,decay, y_uu = 2.0, y_x = 2.0):
 
 
 	if decay == "WBWB":
-		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s."%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x),WBWB_had_BR , Suu_to_chi_chi_to_VLQs_xs*WBWB_had_BR ) )
+		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s"%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x),WBWB_had_BR , np.around(Suu_to_chi_chi_to_VLQs_xs*WBWB_had_BR,2)   ) )
 
 		return Suu_to_chi_chi_to_VLQs_xs*WBWB_had_BR
 	elif decay == "HTHT":
-		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s."%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x), HTHT_had_BR, Suu_to_chi_chi_to_VLQs_xs*HTHT_had_BR ) )
+		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s"%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x), HTHT_had_BR, np.around(Suu_to_chi_chi_to_VLQs_xs*HTHT_had_BR,2)   ) )
 
 		return Suu_to_chi_chi_to_VLQs_xs*HTHT_had_BR
 	elif decay == "ZTZT":
-		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s."%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x), ZTZT_had_BR,Suu_to_chi_chi_to_VLQs_xs*ZTZT_had_BR ) )
+		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s"%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x), ZTZT_had_BR, np.around(Suu_to_chi_chi_to_VLQs_xs*ZTZT_had_BR,2)   ) )
 
 		return Suu_to_chi_chi_to_VLQs_xs*ZTZT_had_BR
 	elif decay == "WBHT":
-		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s."%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x), WBHT_had_BR, Suu_to_chi_chi_to_VLQs_xs*WBHT_had_BR ) )
+		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s"%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x), WBHT_had_BR, np.around(Suu_to_chi_chi_to_VLQs_xs*WBHT_had_BR,2)  ) )
 
 		return Suu_to_chi_chi_to_VLQs_xs*WBHT_had_BR
 	elif decay == "WBZT":
-		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s."%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x), WBZT_had_BR, Suu_to_chi_chi_to_VLQs_xs*WBZT_had_BR ) )
+		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s"%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x), WBZT_had_BR, np.around(Suu_to_chi_chi_to_VLQs_xs*WBZT_had_BR ,2) ) )
 
 		return Suu_to_chi_chi_to_VLQs_xs*WBZT_had_BR
 	elif decay == "HTZT":
-		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s."%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x),HTZT_had_BR,Suu_to_chi_chi_to_VLQs_xs*HTZT_had_BR  ) )
+		print( "mass point is %s, decay is %s, Suu production xs is %s, Suu -> chi chi BR is %s, hadronic BR is %s, total xs is %s"%(mass_point,decay,Suu_prod_xs[ "%s"%Suu_mass ], calculate_Suu_to_chi_chi_BR( Suu_mass, chi_mass, y_uu, y_x),HTZT_had_BR,  np.around(Suu_to_chi_chi_to_VLQs_xs*HTZT_had_BR,2)   ) )
 
 		return Suu_to_chi_chi_to_VLQs_xs*HTZT_had_BR
 	else:
